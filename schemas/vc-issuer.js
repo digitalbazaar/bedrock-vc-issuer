021ed419657f8e865f149b896932d3ab16f43f66
Matthew Collier <mcollier@digitalbazaar.com>
2020-01-15 13:21:04 -0500

    Add tests.

--- schemas/vc-issuer.js
+++ schemas/vc-issuer.js
@@ -3,10 +3,34 @@
  */
 'use strict';
 
-const bedrock = require('bedrock');
+// const bedrock = require('bedrock');
 // const {config} = bedrock;
 // const {schemas} = require('bedrock-validation');
 
-const foo = {};
+const instancesQuery = {
+  title: 'Instances Query',
+  type: 'object',
+  additionalProperties: false,
+  required: ['controller'],
+  properties: {
+    controller: {type: 'string'},
+  }
+};
 
-module.exports.foo = () => foo;
+const instancesCreate = {
+  title: 'Instances Create',
+  type: 'object',
+  additionalProperties: false,
+  required: ['controller', 'id', 'name'],
+  properties: {
+    // urn:uuid:a99bfceb-f888-44f2-9319-d51e36038062
+    controller: {type: 'string'},
+    // uuid
+    id: {type: 'string'},
+    // user supplied string, human readable name of the instance
+    name: {type: 'string'},
+  }
+};
+
+module.exports.instancesCreate = () => instancesCreate;
+module.exports.instancesQuery = () => instancesQuery;
--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -16,9 +16,9 @@ const api = create({
 });
 
 describe('API', () => {
-  describe('instances endpoint', () => {
+  describe('instances GET endpoint', () => {
     describe('unauthenticated', () => {
-      it('GET returns NotAllowedError', async () => {
+      it('returns NotAllowedError', async () => {
         const result = await api.get('/instances');
         should.exist(result.problem);
         result.problem.should.equal('CLIENT_ERROR');
@@ -26,7 +26,48 @@ describe('API', () => {
         should.exist(result.data.type);
         result.data.type.should.equal('NotAllowedError');
       });
-      it('POST returns NotAllowedError', async () => {
+    }); // end unauthenticated
+    describe('authenticated', () => {
+      let passportStub;
+      before(() => {
+        passportStub = sinon.stub(brPassport, 'optionallyAuthenticated');
+        _stubPassport(passportStub);
+      });
+      after(() => {
+        passportStub.restore();
+      });
+      it('ValidationError on missing controller', async () => {
+        const result = await api.get('/instances');
+        should.exist(result.problem);
+        result.problem.should.equal('CLIENT_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('ValidationError');
+      });
+      it('ValidationError on additional query param', async () => {
+        const result = await api.get('/instances', {
+          controller: 'did:v1:nym:abc',
+          foo: 'bar',
+        });
+        should.exist(result.problem);
+        result.problem.should.equal('CLIENT_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('ValidationError');
+      });
+      it('controller with no instances returns empty array', async () => {
+        const params = {controller: 'did:v1:nym:abc'};
+        const result = await api.get('/instances', params);
+        should.not.exist(result.problem);
+        result.data.should.be.an('array');
+        result.data.should.have.length(0);
+      });
+    }); // end authenticated
+  }); // end instances GET
+
+  describe('instances POST endpoint', () => {
+    describe('unauthenticated', () => {
+      it('returns NotAllowedError', async () => {
         const result = await api.post('/instances', {foo: 'bar'});
         should.exist(result.problem);
         result.problem.should.equal('CLIENT_ERROR');
@@ -44,19 +85,96 @@ describe('API', () => {
       after(() => {
         passportStub.restore();
       });
-      it('GET returns ', async () => {
-        const result = await api.get('/instances');
-        should.not.exist(result.problem);
-        console.log('DDDDDdd', result.data);
+      it('NotAllowedError if user is not controller', async () => {
+        const result = await api.post('/instances', {
+          // "controller": "urn:uuid:a99bfceb-f888-44f2-9319-d51e36038062",
+          controller: 'someController',
+          id: '64731d47-b0d2-4157-80b5-2e58ea97d13c',
+          name: 'Test Instance',
+        });
+        should.exist(result.problem);
+        result.problem.should.equal('CLIENT_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('NotAllowedError');
+      });
+      it('no ValidationError on proper instance', async () => {
+        const result = await api.post('/instances', {
+          controller: 'theMockControllerId',
+          id: '64731d47-b0d2-4157-80b5-2e58ea97d13c',
+          name: 'Test Instance',
+        });
+
+        // FIXME: the SERVER_ERROR/InvalidStateError being returned is
+        // expected at this point, schema validation on this API has passed.
+        // this error is being produced by the KMS system because all the
+        // proper mocks are not yet in place
+        should.exist(result.problem);
+        result.problem.should.equal('SERVER_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('InvalidStateError');
+      });
+      it('ValidationError on missing controller', async () => {
+        const result = await api.post('/instances', {
+          // controller: 'theMockControllerId',
+          id: '64731d47-b0d2-4157-80b5-2e58ea97d13c',
+          name: 'Test Instance',
+        });
+        should.exist(result.problem);
+        result.problem.should.equal('CLIENT_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('ValidationError');
+      });
+      it('ValidationError on missing id', async () => {
+        const result = await api.post('/instances', {
+          controller: 'theMockControllerId',
+          // id: '64731d47-b0d2-4157-80b5-2e58ea97d13c',
+          name: 'Test Instance',
+        });
+        should.exist(result.problem);
+        result.problem.should.equal('CLIENT_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('ValidationError');
+      });
+      it('ValidationError on missing name', async () => {
+        const result = await api.post('/instances', {
+          controller: 'theMockControllerId',
+          id: '64731d47-b0d2-4157-80b5-2e58ea97d13c',
+          // name: 'Test Instance',
+        });
+        should.exist(result.problem);
+        result.problem.should.equal('CLIENT_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('ValidationError');
+      });
+      it('ValidationError on additional field', async () => {
+        const result = await api.post('/instances', {
+          controller: 'theMockControllerId',
+          id: '64731d47-b0d2-4157-80b5-2e58ea97d13c',
+          name: 'Test Instance',
+          foo: 'bar',
+        });
+        should.exist(result.problem);
+        result.problem.should.equal('CLIENT_ERROR');
+        should.exist(result.data);
+        should.exist(result.data.type);
+        result.data.type.should.equal('ValidationError');
       });
     }); // end authenticated
-  });
+  }); // end instances POST
 });
 
 function _stubPassport(passportStub) {
   passportStub.callsFake((req, res, next) => {
     req.user = {
-      account: {}
+      account: {},
+      actor: {
+        id: 'theMockControllerId'
+      }
     };
     next();
   });
