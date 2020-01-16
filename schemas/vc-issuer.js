6f4df6af8b5568385b4cee82076c41611d1de3ce
Andrew Jones <ajones@digitalbazaar.com>
2020-01-16 17:45:59 -0500

    Add an instance update validator.

--- schemas/vc-issuer.js
+++ schemas/vc-issuer.js
@@ -3,9 +3,7 @@
  */
 'use strict';
 
-// const bedrock = require('bedrock');
-// const {config} = bedrock;
-// const {schemas} = require('bedrock-validation');
+const {schemas} = require('bedrock-validation');
 const {presentation} = require('./subs/presentation');
 const {
   authenticationProof,
@@ -134,7 +132,56 @@ CREATE ISSUER JSON {
 */
 
 const instancesUpdate = {
-
+  title: 'Issuer Instance Update',
+  type: 'object',
+  required: ['controller', 'presentation'],
+  properties: {
+    controller: {
+      type: 'string',
+      title: 'The Update Controller',
+      examples: [
+        'urn:uuid:a99bfceb-f888-44f2-9319-d51e36038062'
+      ],
+      // FIXME add a regex for controller ids in veres-issuer
+      pattern: '^(.*)$'
+    },
+    presentation: {
+      title: 'Issuer Instance Presentation',
+      type: 'object',
+      required: [
+        '@context',
+        'type',
+        'holder',
+        'capability',
+        'proof'
+      ],
+      properties: {
+        '@context': schemas.jsonldContext(),
+        type: {
+          $id: '#/properties/presentation/properties/type',
+          type: 'string',
+          title: 'The Type Schema',
+          examples: ['VerifiablePresentation'],
+          pattern: '^(.*)$'
+        },
+        holder: {
+          $id: '#/properties/presentation/properties/holder',
+          type: 'string',
+          title: 'The Holder Schema',
+          examples: [
+            'did:v1:test:nym:z6MkjrFvxZyHF6pNLqMynYSfL14oUkSPvENaqCUE2ygrtDNV'
+          ],
+          pattern: '^(.*)$'
+        },
+        capability: {
+          type: 'array',
+          title: 'Capability',
+          items: delegationZCap()
+        },
+        proof: authenticationProof()
+      }
+    }
+  }
 };
 
 module.exports.instancesUpdate = () => instancesUpdate;
