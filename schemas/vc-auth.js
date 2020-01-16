950abafd935e5e5757f8718204916223fa8bc76a
Andrew Jones <ajones@digitalbazaar.com>
2020-01-16 17:52:14 -0500

    Remove example json from validators.

--- schemas/vc-auth.js
+++ schemas/vc-auth.js
@@ -6,26 +6,6 @@
 const {schemas} = require('bedrock-validation');
 const {authenticationProof} = require('./subs/vc-common');
 
-/**
- *  DID AUTH REQ BODY workerPid=19432, details={
-  "body": {
-    "presentation": {
-      "@context": "https://www.w3.org/2018/credentials/v1",
-      "type": "VerifiablePresentation",
-      "holder": "did:v1:test:nym:z6Mkm8ukp7U3Vnz5tengH9tNyUcR9FkkzhE7AXZuebdYsKbe",
-      "proof": {
-        "type": "Ed25519Signature2018",
-        "created": "2020-06-18T21:19:10Z",
-        "proofPurpose": "authentication",
-        "verificationMethod": "did:v1:test:nym:z6Mkm8ukp7U3Vnz5tengH9tNyUcR9FkkzhE7AXZuebdYsKbe#key",
-        "challenge": "fb990197-c8f9-4bf7-8313-e8abe4c94792",
-        "jws": "BavEll0/I1..W3JT24="
-      }
-    }
-  }
-}
-*/
-
 const login = {
   type: 'object',
   title: 'DID Login',
@@ -37,12 +17,8 @@ const login = {
       required: ['@context', 'type', 'holder', 'proof'],
       properties: {
         '@context': schemas.jsonldContext(),
-        type: {
-          type: 'string'
-        },
-        holder: {
-          type: 'string'
-        },
+        type: {type: 'string'},
+        holder: {type: 'string'},
         proof: authenticationProof()
       }
     }
