16de4dd32860e44c2994e7ed7789c25943e44562
Andrew L Jones <ajones@digitalbazaar.com>
2020-04-22 08:32:14 -0400

    Start creating the user profile edv in helpers.

--- test/mocha/helpers.js
+++ test/mocha/helpers.js
@@ -20,38 +20,24 @@ async function insertIssuerAgent({id, token}) {
   // this is the profile associated with an issuer account
   const {id: profileId} = await profiles.create({accountId: id});
   const profileAgentRecord = await profileAgents.getByProfile(
-    {profileId, accountId: id, includeSecrets: true})
+    {profileId, accountId: id, includeSecrets: true});
   const {profileAgent, secrets} = profileAgentRecord;
-  const {profileCapabilityInvocationKey} = profileAgent.zcaps;
+  // we will need this to delegate and invoke
   const invocationSigner = await profileAgents.getSigner({profileAgentRecord});
   const {capabilityAgent, keystoreAgent} = await profileAgents.getAgents(
     {profileAgent, secrets});
-  const [keyAgreementKey, hmac] = await Promise.all([
-    keystoreAgent.generateKey({
-      type: 'keyAgreement',
-      kmsModule
-    }),
-    keystoreAgent.generateKey({
-      type: 'hmac',
-      kmsModule
-    })
-  ]);
-console.log('profile\'s profileAgent', keyAgreementKey, hmac);
+  // creates an edv for the profile-agent-edv-document
+  const profileAgentEdvDocument = 'profile-agent-edv-document';
   // this is the userProfileEdv usually created in the wallet.
-  const edvId = `${baseUri}/edvs/${(await edvHelpers.generateRandom())}`;
-  const config = {
-    id: edvId,
-    sequence: 0,
-    controller: profileId,
-    referenceId: 'vc-issuer-test-user',
-    keyAgreementKey: {id: keyAgreementKey.id, type: keyAgreementKey.type},
-    hmac: {id: hmac.id, type: hmac.type}
-  };
-  const result = await edvStorage.insertConfig({actor: null, config});
-console.log('edv storage result', result);
-
+  const {edvId} = await createProfileEdv({
+    profileId,
+    referenceId: profileAgentEdvDocument,
+    keystoreAgent
+  });
+  // now that we have an edv for the test
+  // we need to delegate read only access to us.
   const delegateEdvDocumentRequest = {
-    referenceId: `profile-agent-edv-document`,
+    referenceId: profileAgentEdvDocument,
     // the profile agent is only allowed to read its own doc
     allowedAction: ['read'],
     controller: profileId,
@@ -62,6 +48,53 @@ console.log('edv storage result', result);
   // this is the profileAgent used to issue a credential.
   const integration = await profileAgents.create({profileId, token});
   return {instance: {id: profileId}, integration};
+}
+
+async function createProfileEdv({profileId, referenceId, keystoreAgent}) {
+  const [keyAgreementKey, hmac] = await Promise.all([
+    keystoreAgent.generateKey({
+      type: 'keyAgreement',
+      kmsModule
+    }),
+    keystoreAgent.generateKey({
+      type: 'hmac',
+      kmsModule
+    })
+  ]);
+  const edvId = `${baseUri}/edvs/${(await edvHelpers.generateRandom())}`;
+  const config = {
+    id: edvId,
+    sequence: 0,
+    controller: profileId,
+    referenceId,
+    keyAgreementKey: {id: keyAgreementKey.id, type: keyAgreementKey.type},
+    hmac: {id: hmac.id, type: hmac.type}
+  };
+  const result = await edvStorage.insertConfig({actor: null, config});
+  return {keyAgreementKey, hmac, config: result, edvId};
+}
+
+// this will create the user edv and store
+// the necessary zcaps in it used by the instance's issuer
+async function initializeAccessManagement({
+  profileId,
+  profileContent,
+  edvId,
+  hmac,
+  keyAgreementKey,
+  indexes: []
+}) {
+  // create access management info
+  const accessManagement = {
+    hmac: {id: hmac.id, type: hmac.type},
+    keyAgreementKey: {id: keyAgreementKey.id, type: keyAgreementKey.type},
+    indexes: [
+      {attribute: 'content.id', unique: true},
+      {attribute: 'content.type'},
+      ...indexes
+    ],
+    zcaps: {}
+  };
 }
 
 exports.insertIssuerAgent = insertIssuerAgent;
