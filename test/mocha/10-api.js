baee6d96796bf65dda54702a9951836d686ae1dc
Andrew L Jones <ajones@digitalbazaar.com>
2020-06-08 16:18:16 -0400

    Add further sinon stubs to keep mocha from failing post test run.

--- test/mocha/10-api.js
+++ test/mocha/10-api.js
@@ -8,6 +8,7 @@ const {create} = require('apisauce');
 const {httpsAgent} = require('bedrock-https-agent');
 const helpers = require('./helpers.js');
 const sinon = require('sinon');
+const Runner = require('mocha/lib/runner');
 const brPassport = require('bedrock-passport');
 
 const api = create({
@@ -24,6 +25,12 @@ describe('API', function() {
       const accountId = 'urn:uuid:e9b57b37-2fea-43d6-82cb-f4a02c144e38';
       agents = await helpers.insertIssuerAgent(
         {id: accountId, token: 'test-token'});
+      if(Runner.prototype.hasOwnProperty('uncaughtEnd')) {
+        sinon.stub(Runner.prototype, 'uncaughtEnd').returns(true);
+      }
+      if(Runner.prototype.hasOwnProperty('_uncaught')) {
+        sinon.stub(Runner.prototype, '_uncaught').returns(true);
+      }
     });
     it('should issue a credential', async function() {
       const {integration: {secrets}} = agents;
