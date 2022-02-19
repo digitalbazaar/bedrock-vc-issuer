07a4bee99afe1d0c76b9e2fb89e54916e6e6db8f
Dave Longley <dlongley@digitalbazaar.com>
2022-02-18 19:29:26 -0500

    Update test config.

--- test/test.config.js
+++ test/test.config.js
@@ -1,22 +1,35 @@
 /*!
- * Copyright (c) 2020 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2022 Digital Bazaar, Inc. All rights reserved.
  */
 'use strict';
 
 const {config} = require('bedrock');
 const path = require('path');
-
-config.mocha.tests.push(path.join(__dirname, 'mocha'));
+require('bedrock-vc-issuer');
 
 // MongoDB
 config.mongodb.name = 'bedrock_vc_issuer_test';
 config.mongodb.dropCollections.onInit = true;
 config.mongodb.dropCollections.collections = [];
+// drop all collections on initialization
+config.mongodb.dropCollections = {};
+config.mongodb.dropCollections.onInit = true;
+config.mongodb.dropCollections.collections = [];
 
-config.kms.allowedHost = config.server.host;
-
-config['https-agent'].rejectUnauthorized = false;
-config.kms.allowedHost = config.server.host;
-config['kms-http'].requireAuthentication = false;
-config.kmsModule = 'ssm-v1';
 config.mocha.options.fullTrace = true;
+config.mocha.tests.push(path.join(__dirname, 'mocha'));
+
+// allow self-signed certs in test framework
+config['https-agent'].rejectUnauthorized = false;
+
+// create test application identity
+// ...and `ensureConfigOverride` has already been set via
+// `bedrock-app-identity` so it doesn't have to be set here
+config['app-identity'].seeds.services['vc-issuer'] = {
+  id: 'did:key:z6MkrH839XwPCUQ2TkA6ifehciWnEvzuQ2njc6J19fpuP5oN',
+  seedMultibase: 'z1AgvAGfbairK3AV6GqbeF8gSpYZXftQsGb5DTjptgawNyn',
+  serviceType: 'vc-issuer'
+};
+
+// use local KMS for testing
+config['service-agent'].kms.baseUrl = 'https://localhost:18443/kms';
