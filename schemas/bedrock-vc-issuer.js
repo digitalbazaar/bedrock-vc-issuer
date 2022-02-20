21115fbd192dd0f4ab07de772c671fb295dc2e19
Dave Longley <dlongley@digitalbazaar.com>
2022-02-20 18:53:51 -0500

    Update main http service and related modules.

--- schemas/bedrock-vc-issuer.js
+++ schemas/bedrock-vc-issuer.js
@@ -1,6 +1,15 @@
 /*!
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
+const context = {
+  title: '@context',
+  type: 'array',
+  minItems: 1,
+  items: {
+    type: ['string', 'object']
+  }
+};
+
 export const issueOptions = {
   title: 'Issue Options',
   type: 'object',
@@ -35,4 +44,54 @@ export const statusListOptions = {
   items: {
     statusListConfig
   }
+};
+
+export const issueCredentialBody = {
+  title: 'Issue Credential',
+  type: 'object',
+  required: ['credential'],
+  additionalProperties: false,
+  properties: {
+    options: {
+      // FIXME: make restricted
+      type: 'object'
+    },
+    credential: {
+      type: 'object',
+      additionalProperties: true,
+      required: ['@context'],
+      properties: {
+        '@context': context
+      }
+    }
+  }
+};
+
+export const updateCredentialStatusBody = {
+  title: 'Update Credential Status',
+  type: 'object',
+  required: ['credentialId', 'credentialStatus'],
+  additionalProperties: false,
+  properties: {
+    credentialId: {
+      type: 'string'
+    },
+    credentialStatus: {
+      type: 'object',
+      additionalProperties: false,
+      properties: {
+        type: {
+          type: 'string'
+        }
+      }
+    }
+  }
+};
+
+export const publishSlcBody = {
+  title: 'Publish Status List Credential',
+  type: 'object',
+  additionalProperties: false,
+  // body must be empty
+  properties: {}
 };
