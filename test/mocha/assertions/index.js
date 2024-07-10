f5aa39d4bd8275244f4937565dc4085afea8f015
Dave Longley <dlongley@digitalbazaar.com>
2024-07-09 22:41:06 -0400

    Import `randomUUID`.

--- test/mocha/assertions/index.js
+++ test/mocha/assertions/index.js
@@ -3,6 +3,7 @@
  */
 import {CapabilityAgent} from '@digitalbazaar/webkms-client';
 import {createZcapClient} from '../helpers.js';
+import {randomUUID} from 'node:crypto';
 
 const VC_CONTEXT_V1 = 'https://www.w3.org/2018/credentials/v1';
 const VC_CONTEXT_V2 = 'https://www.w3.org/ns/credentials/v2';
@@ -28,7 +29,7 @@ export async function assertStoredCredential({
     let error;
     let result;
     try {
-      const secret = crypto.randomUUID();
+      const secret = randomUUID();
       const handle = 'test';
       const capabilityAgent = await CapabilityAgent.fromSecret({
         secret, handle
