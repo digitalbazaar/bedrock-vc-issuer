99d6e295ddfac98501f18eb249a3316a7ddec3df
Dave Longley <dlongley@digitalbazaar.com>
2025-09-09 11:49:58 -0400

    Ensure `options.credentialId` uniqueness is enforced.
    - When providing `options.credentialId`, the VC (or minimally, its
      `credentialId`) will be stored, even if no credential status is
      configured, so that uniqueness can be enforced on
      `options.credentialId` alone.

--- test/mocha/assertions/issueWithoutStatus.js
+++ test/mocha/assertions/issueWithoutStatus.js
@@ -4,6 +4,7 @@
 import * as assertions from './index.js';
 import * as helpers from '../helpers.js';
 import {createRequire} from 'node:module';
+import {randomUUID as uuid} from 'node:crypto';
 
 const require = createRequire(import.meta.url);
 
@@ -83,6 +84,105 @@ export function testIssueWithoutStatus({
         // `created` should not be set by default because new issue config
         // mechanism was used w/o requesting it
         should.not.exist(verifiableCredential.proof.created);
+      }
+    });
+    it('issues twice w/o "credentialId" and no duplicate error', async () => {
+      // reuse `credentialId` internal to VC but NOT `options.credentialId` to
+      // ensure no storage or uniqueness constraint is applied
+      const credentialId = `urn:uuid:${uuid()}`;
+      for(let i = 0; i < 2; ++i) {
+        const credential = structuredClone(mockCredentialV2);
+        credential.id = credentialId;
+        const zcapClient = helpers.createZcapClient({capabilityAgent});
+        const {verifiableCredential} = await assertions.issueAndAssert({
+          configId: noStatusListIssuerId,
+          credential,
+          issueOptions,
+          zcapClient,
+          capability: noStatusListIssuerRootZcap
+        });
+        should.exist(verifiableCredential.id);
+        should.not.exist(verifiableCredential.credentialStatus);
+      }
+    });
+    it('fails to issue with a duplicate "credentialId"', async () => {
+      const zcapClient = helpers.createZcapClient({capabilityAgent});
+
+      // issue VC without `id` and no `credentialId` option
+      // (should succeed)
+      {
+        const credential = structuredClone(mockCredentialV2);
+        delete credential.id;
+        let error;
+        let result;
+        try {
+          result = await zcapClient.write({
+            url: `${noStatusListIssuerId}/credentials/issue`,
+            capability: noStatusListIssuerRootZcap,
+            json: {
+              credential,
+              options: issueOptions
+            }
+          });
+        } catch(e) {
+          error = e;
+        }
+        assertNoError(error);
+        should.exist(result.data);
+        should.exist(result.data.verifiableCredential);
+        const {verifiableCredential} = result.data;
+        const {proof} = verifiableCredential;
+        should.exist(proof);
+      }
+
+      // issue VC with "credentialId" option only (should succeed)
+      const credentialId = `urn:uuid:${uuid()}`;
+      {
+        const credential = structuredClone(mockCredentialV2);
+        delete credential.id;
+        let error;
+        let result;
+        try {
+          result = await zcapClient.write({
+            url: `${noStatusListIssuerId}/credentials/issue`,
+            capability: noStatusListIssuerRootZcap,
+            json: {
+              credential,
+              options: {...issueOptions, credentialId}
+            }
+          });
+        } catch(e) {
+          error = e;
+        }
+        assertNoError(error);
+        should.exist(result.data);
+        should.exist(result.data.verifiableCredential);
+        const {verifiableCredential} = result.data;
+        const {proof} = verifiableCredential;
+        should.exist(proof);
+      }
+
+      // issue VC with the same "credentialId" again (should fail)
+      {
+        const credential = structuredClone(mockCredentialV2);
+        delete credential.id;
+        let error;
+        let result;
+        try {
+          result = await zcapClient.write({
+            url: `${noStatusListIssuerId}/credentials/issue`,
+            capability: noStatusListIssuerRootZcap,
+            json: {
+              credential,
+              options: {...issueOptions, credentialId}
+            }
+          });
+        } catch(e) {
+          error = e;
+        }
+        should.exist(error);
+        error.data.type.should.equal('DuplicateError');
+        should.not.exist(result);
       }
     });
     it('issues a valid credential w/"@language"+"@dir"', async () => {
